f6899b0b7137292ce7afba9e5ae9187c4aeda45b
Firmansyah Yanuar <fyfirman@gmail.com>
2019-10-09 15:31:10 +0700

    Add movie list to fragment

--- app/src/main/java/com/fyfirman/moviecatalogue/MainActivity.java
+++ app/src/main/java/com/fyfirman/moviecatalogue/MainActivity.java
@@ -13,12 +13,6 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] dataTitle;
-    private String[] dataSynopsis;
-    private TypedArray dataPhoto;
-    private MovieAdapter movieAdapter;
-    private ArrayList<Movie> movies;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,21 +20,6 @@ public class MainActivity extends AppCompatActivity {
 
         initTabLayout();
 
-        movieAdapter = new MovieAdapter(this);
-//        ListView listView = findViewById(R.id.movie_list);
-//        listView.setAdapter(movieAdapter);
-//
-//        prepare();
-//        addItem();
-//
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Intent showDetailMovie = new Intent(MainActivity.this, DetailMovieActivity.class);
-//                showDetailMovie.putExtra(DetailMovieActivity.EXTRA_MOVIE, movies.get(i));
-//                startActivity(showDetailMovie);
-//            }
-//        });
 
     }
 
@@ -65,22 +44,5 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(mainFragmentPagerAdapter);
     }
 
-    private void prepare() {
-        dataTitle = getResources().getStringArray(R.array.data_title);
-        dataSynopsis = getResources().getStringArray(R.array.data_synopsis);
-        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
-    }
 
-    private void addItem(){
-        movies = new ArrayList<Movie>();
-
-        for (int i = 0; i < dataTitle.length; i++){
-            Movie movie = new Movie();
-            movie.setPhoto(dataPhoto.getResourceId(i,1));
-            movie.setTitle(dataTitle[i]);
-            movie.setSynopsis(dataSynopsis[i]);
-            movies.add(movie);
-        }
-        movieAdapter.setMovies(movies);
-    }
 }
--- app/src/main/java/com/fyfirman/moviecatalogue/MoviesFragment.java
+++ app/src/main/java/com/fyfirman/moviecatalogue/MoviesFragment.java
@@ -1,19 +1,69 @@
 package com.fyfirman.moviecatalogue;
 
+import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
 
 public class MoviesFragment extends Fragment {
+  private String[] dataTitle;
+  private String[] dataSynopsis;
+  private TypedArray dataPhoto;
+  private MovieAdapter movieAdapter;
+  private ArrayList<Movie> movies;
 
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_movies, container, false);
 
+    movieAdapter = new MovieAdapter(getActivity());
+
+    ListView listView = view.findViewById(R.id.movie_list);
+    listView.setAdapter(movieAdapter);
+
+    getResourceFromView();
+    addItem();
+
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//        Toast toast = Toast.makeText(getActivity(),"Hello Movies!",Toast.LENGTH_SHORT);
+//        toast.show();
+          Intent showDetailMovie = new Intent(getActivity(), DetailMovieActivity.class);
+          showDetailMovie.putExtra(DetailMovieActivity.EXTRA_MOVIE, movies.get(i));
+          startActivity(showDetailMovie);
+      }
+    });
+
     return view;
+  }
+
+
+  private void getResourceFromView() {
+    dataTitle = getResources().getStringArray(R.array.data_title);
+    dataSynopsis = getResources().getStringArray(R.array.data_synopsis);
+    dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+  }
+
+  private void addItem(){
+    movies = new ArrayList<Movie>();
+
+    for (int i = 0; i < dataTitle.length; i++){
+      Movie movie = new Movie();
+      movie.setPhoto(dataPhoto.getResourceId(i,1));
+      movie.setTitle(dataTitle[i]);
+      movie.setSynopsis(dataSynopsis[i]);
+      movies.add(movie);
+    }
+    movieAdapter.setMovies(movies);
   }
 }
