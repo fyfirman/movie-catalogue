2aa13c76a49d0917c0a636153be758c94d0b2f79
Firmansyah Yanuar <fyfirman@gmail.com>
2019-11-13 21:21:39 +0700

    Add search feature on viewModel

--- app/src/main/java/com/fyfirman/moviecatalogue/viewmodel/MovieViewModel.java
+++ app/src/main/java/com/fyfirman/moviecatalogue/viewmodel/MovieViewModel.java
@@ -15,6 +15,7 @@ import org.json.JSONObject;
 public class MovieViewModel extends ViewModel {
 
   private MutableLiveData<ArrayList<Movie>> listMovies = new MutableLiveData<>();
+  String api_key = "b8d05a72822a6e82cf552ec84fc42c85";
 
   public void setData(String language) {
     final ArrayList<Movie> listItems = new ArrayList<>();
@@ -22,40 +23,84 @@ public class MovieViewModel extends ViewModel {
     AsyncHttpClient client = new AsyncHttpClient();
 
     String base_url = "https://api.themoviedb.org/3/discover/movie?";
-    String api_key = "b8d05a72822a6e82cf552ec84fc42c85";
 
-    client.get(base_url + "api_key=" + api_key + "&language=" + language, new AsyncHttpResponseHandler() {
+    client.get(base_url + "api_key=" + api_key + "&language=" + language,
+        new AsyncHttpResponseHandler() {
+          @Override
+          public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+            try {
+              String result = new String(responseBody);
+
+              Log.d("Response", result);
+
+              JSONObject responseObject = new JSONObject(result);
+              JSONArray list = responseObject.getJSONArray("results");
+
+              for (int i = 0; i < list.length(); i++) {
+                JSONObject jsonObject = list.getJSONObject(i);
+
+                Movie movie = new Movie();
+                movie.setId(jsonObject.getInt("id"));
+                movie.setTitle(jsonObject.getString("title"));
+                movie.setOverview(jsonObject.getString("overview"));
+                movie.setPoster_path(jsonObject.getString("poster_path"));
+
+                listItems.add(movie);
+              }
+              listMovies.postValue(listItems);
+            } catch (Exception e) {
+              Log.d("Exception", e.getMessage());
+            }
+          }
+
+          @Override
+          public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
+              Throwable error) {
+            Log.d("onFailure", error.getMessage());
+//        TODO: Add toast
+//        Toast.makeText(context, context.getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
+          }
+        });
+  }
+
+  //  public void searchMovie(final Context context, String query) {
+  public void searchMovie(String query) {
+    AsyncHttpClient client = new AsyncHttpClient();
+    final ArrayList<Movie> listItems = new ArrayList<>();
+    String url =
+        "https://api.themoviedb.org/3/search/movie?api_key=" + api_key + "&language=en-US&query="
+            + query;
+
+    client.get(url, new AsyncHttpResponseHandler() {
       @Override
       public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
         try {
           String result = new String(responseBody);
-
-          Log.d("Response", result);
-
           JSONObject responseObject = new JSONObject(result);
           JSONArray list = responseObject.getJSONArray("results");
 
           for (int i = 0; i < list.length(); i++) {
             JSONObject jsonObject = list.getJSONObject(i);
-
             Movie movie = new Movie();
             movie.setId(jsonObject.getInt("id"));
             movie.setTitle(jsonObject.getString("title"));
             movie.setOverview(jsonObject.getString("overview"));
             movie.setPoster_path(jsonObject.getString("poster_path"));
-
             listItems.add(movie);
           }
+
           listMovies.postValue(listItems);
         } catch (Exception e) {
-          Log.d("Exception", e.getMessage());
+          Log.d("searchMovieErr", e.getMessage());
         }
       }
 
       @Override
       public void onFailure(int statusCode, Header[] headers, byte[] responseBody,
           Throwable error) {
-        Log.d("onFailure", error.getMessage());
+//        Toast.makeText(context, context.getString(R.string.connection_error), Toast.LENGTH_LONG)
+//            .show();
+        Log.d("searchMovieNoConn", error.getMessage());
       }
     });
   }
