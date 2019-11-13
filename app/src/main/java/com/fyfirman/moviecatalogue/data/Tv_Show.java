1073f59796aad26963f090d6fdda8c6250741558
Firmansyah Yanuar <fyfirman@gmail.com>
2019-11-13 07:18:42 +0700

    Update Data Model

--- app/src/main/java/com/fyfirman/moviecatalogue/data/Tv_Show.java
+++ app/src/main/java/com/fyfirman/moviecatalogue/data/Tv_Show.java
@@ -4,31 +4,24 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Tv_Show implements Parcelable {
+
   final String base_url = "https://image.tmdb.org/t/p/";
   final String poster_size = "w185/";
 
+  Integer id;
   private String poster_path;
   private String title;
-
-  public String getOverview() {
-    return overview;
-  }
-
-  public void setOverview(String overview) {
-    this.overview = overview;
-  }
-
   private String overview;
 
   public Tv_Show() {
   }
 
-  public String getPoster_path() {
-    return base_url+poster_size+poster_path;
+  public Integer getId() {
+    return id;
   }
 
-  public void setPoster_path(String poster_path) {
-    this.poster_path = poster_path;
+  public void setId(Integer id) {
+    this.id = id;
   }
 
   public String getTitle() {
@@ -39,6 +32,22 @@ public class Tv_Show implements Parcelable {
     this.title = title;
   }
 
+  public String getOverview() {
+    return overview;
+  }
+
+  public void setOverview(String overview) {
+    this.overview = overview;
+  }
+
+  public String getPoster_path() {
+    return base_url + poster_size + poster_path;
+  }
+
+  public void setPoster_path(String poster_path) {
+    this.poster_path = poster_path;
+  }
+
   @Override
   public int describeContents() {
     return 0;
@@ -46,12 +55,14 @@ public class Tv_Show implements Parcelable {
 
   @Override
   public void writeToParcel(Parcel dest, int flags) {
+    dest.writeInt(this.id);
     dest.writeString(this.title);
     dest.writeString(this.overview);
     dest.writeString(this.poster_path);
   }
 
   protected Tv_Show(Parcel in) {
+    this.id = in.readInt();
     this.title = in.readString();
     this.overview = in.readString();
     this.poster_path = in.readString();
