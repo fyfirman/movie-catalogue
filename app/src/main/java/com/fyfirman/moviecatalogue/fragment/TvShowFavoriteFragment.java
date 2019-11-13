9d97188bd5aee6ff45254fbc2b5c5a0e371c8a74
Firmansyah Yanuar <fyfirman@gmail.com>
2019-11-13 07:10:41 +0700

    Update TvShowFavoriteFragment

--- app/src/main/java/com/fyfirman/moviecatalogue/fragment/TvShowFavoriteFragment.java
+++ app/src/main/java/com/fyfirman/moviecatalogue/fragment/TvShowFavoriteFragment.java
@@ -1,7 +1,5 @@
 package com.fyfirman.moviecatalogue.fragment;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,26 +13,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import com.fyfirman.moviecatalogue.R;
-import com.fyfirman.moviecatalogue.activity.DetailTvShowActivity;
+import com.fyfirman.moviecatalogue.activity.DetailMovieActivity;
 import com.fyfirman.moviecatalogue.adapter.ListTvShowAdapter;
+import com.fyfirman.moviecatalogue.adapter.MovieAdapter;
 import com.fyfirman.moviecatalogue.data.Tv_Show;
-import com.fyfirman.moviecatalogue.viewmodel.TvShowViewModel;
+import com.fyfirman.moviecatalogue.database.MovieFavoriteHelper;
 import java.util.ArrayList;
 
 public class TvShowFavoriteFragment extends Fragment {
 
-  private RecyclerView rvTvShowFavorite;
-  private ArrayList<Tv_Show> listTvShowFavorite = new ArrayList<>();
-  private TvShowViewModel tvShowViewModel;
-  private ListTvShowAdapter listTvShowAdapter;
+  private static final String EXTRA_STATE = "EXTRA_STATE";
+  private RecyclerView rvFavTvShow;
+  private ArrayList<Tv_Show> listTvShow = new ArrayList<>();
+  private ListTvShowAdapter tvShowAdapter;
   private ProgressBar progressBar;
+  private TvShowFavoriteHelper tvShowFavoriteHelper;
 
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_tv_show_favorite, container, false);
-    showRecyclerList(view);
+    View view = inflater.inflate(R.layout.fragment_movies_favorite, container, false);
 
     return view;
   }
@@ -48,53 +47,51 @@ public class TvShowFavoriteFragment extends Fragment {
 
     showRecyclerList(view);
 
+    movieFavoriteHelper = MovieFavoriteHelper.getInstance(getContext());
+
+    if (savedInstanceState == null) {
+      new LoadTvShowFavoriteData().execute();
+    } else {
+      ArrayList<Tv_Show> tv_show = savedInstanceState.getParcelableArrayList(EXTRA_STATE);
+      if (tv_show != null) {
+        tvShowAdapter.setData(tv_show);
+        tvShowAdapter.notifyDataSetChanged();
+      }
+    }
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    new LoadTvShowFavoriteData().execute();
   }
 
   private void showRecyclerList(View view) {
     //mencari referensi recycle view
-    rvTvShowFavorite = view.findViewById(R.id.rv_tv_show_fav);
-    rvTvShowFavorite.setHasFixedSize(true);
+    rvFavTvShow = view.findViewById(R.id.rv_movie_favorite);
+    rvFavTvShow.setHasFixedSize(true);
 
     //set layout manager
-    rvTvShowFavorite.setLayoutManager(new LinearLayoutManager(getActivity()));
+    rvFavTvShow.setLayoutManager(new LinearLayoutManager(getActivity()));
 
     //set adapter
-    listTvShowAdapter = new ListTvShowAdapter(getContext(), listTvShowFavorite);
-    rvTvShowFavorite.setAdapter(listTvShowAdapter);
+    tvShowAdapter = new ListTvShowAdapter(getContext(), listTvShow);
+    rvFavTvShow.setAdapter(tvShowAdapter);
 
     //buat onclick callback function
-    listTvShowAdapter.setOnItemClickCallback(new ListTvShowAdapter.OnItemClickCallback() {
+    tvShowAdapter.setOnItemClickCallback(new ListTvShowAdapter.OnItemClickCallback() {
       @Override
       public void onItemClicked(Tv_Show data) {
-        showSelectedTvShow(data);
-      }
-    });
-
-    tvShowViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory())
-        .get(TvShowViewModel.class);
-
-    tvShowViewModel.setData(getResources().getString(R.string.language));
-
-//    Observe
-    tvShowViewModel.getData().observe(this, new Observer<ArrayList<Tv_Show>>() {
-      @Override
-      public void onChanged(ArrayList<Tv_Show> mModels) {
-        if (mModels != null) {
-          listTvShowAdapter.setData(mModels);
-          showLoading(false);
-        }
+        showSelectedMovie(data);
       }
     });
   }
 
-  private void showSelectedTvShow(Tv_Show data) {
-//    Toast toast = Toast.makeText(getActivity(),"Hello TV Show!",Toast.LENGTH_SHORT);
-//    toast.show();
+  private void showSelectedMovie(Tv_Show data) {
+    Intent showDetailMovie = new Intent(getActivity(), DetailMovieActivity.class);
 
-    Intent showDetailTvShow = new Intent(getActivity(), DetailTvShowActivity.class);
-
-    showDetailTvShow.putExtra(DetailTvShowActivity.EXTRA_TV_SHOW, data);
-    startActivity(showDetailTvShow);
+    showDetailMovie.putExtra(DetailMovieActivity.EXTRA_MOVIE, data);
+    startActivity(showDetailMovie);
   }
 
   private void showLoading(Boolean state) {
@@ -105,25 +102,25 @@ public class TvShowFavoriteFragment extends Fragment {
     }
   }
 
-  public class LoadTVFavoriteData extends AsyncTask<Void, Void, Void> {
+  public class LoadTvShowFavoriteData extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPreExecute() {
       super.onPreExecute();
-      progressBar.setVisibility(View.VISIBLE);
+      showLoading(true);
     }
 
     @Override
     protected Void doInBackground(Void... voids) {
-//      TODO: Buat tvFavoriteHelper
-//      listTvShowAdapter.setData(tvFavoriteHelper.selectTV());
+      ArrayList<Tv_Show> data = tvShowFavoriteHelper.selectTvShow();
+      tvShowAdapter.setData(data);
       return null;
     }
 
     @Override
     protected void onPostExecute(Void aVoid) {
       super.onPostExecute(aVoid);
-      progressBar.setVisibility(View.GONE);
-
+      tvShowAdapter.notifyDataSetChanged();
+      showLoading(false);
     }
   }
 }
