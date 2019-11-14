84c930c7a30728a7f4e695a667f88f80bfc94f95
Firmansyah Yanuar <fyfirman@gmail.com>
2019-11-14 15:36:53 +0700

    Add notification feature

--- app/src/main/java/com/fyfirman/moviecatalogue/activity/ReminderActivity.java
+++ app/src/main/java/com/fyfirman/moviecatalogue/activity/ReminderActivity.java
@@ -1,14 +1,18 @@
 package com.fyfirman.moviecatalogue.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import static com.loopj.android.http.AsyncHttpClient.log;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.Toast;
 import com.fyfirman.moviecatalogue.R;
+import com.fyfirman.moviecatalogue.feature.reminder.ReminderReceiver;
 
-public class ReminderActivity extends AppCompatActivity {
+public class ReminderActivity extends AppCompatActivity implements View.OnClickListener {
 
+  private ReminderReceiver reminderReceiver;
   Switch switchDaily, switchNewRelease;
 
   @Override
@@ -16,28 +20,41 @@ public class ReminderActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_reminder);
     setTitle("R.string.reminder");
+
+    reminderReceiver = new ReminderReceiver();
+
 //TODO: Remove hardcoded string
     switchDaily = findViewById(R.id.switch_daily);
     switchNewRelease = findViewById(R.id.switch_new_release);
 
-    switchDaily.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) {
-        if (switchDaily.isChecked()) {
-          Toast.makeText(ReminderActivity.this, "getString(R.string.daily_enabled)", Toast.LENGTH_SHORT).show();
-        } else {
-          Toast.makeText(ReminderActivity.this, "getString(R.string.daily_disabled)", Toast.LENGTH_SHORT).show();
-        }
-      }
-    });
+    switchDaily.setOnClickListener(this);
 
-    switchNewRelease.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) {
-        if (switchNewRelease.isChecked()) {
-          Toast.makeText(ReminderActivity.this, "getString(R.string.new_release_enabled)", Toast.LENGTH_SHORT).show();
+    switchNewRelease.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.switch_daily:
+        if (switchDaily.isChecked()) {
+          Toast.makeText(this, "getString(R.string.daily_enabled)", Toast.LENGTH_SHORT).show();
+          reminderReceiver.setDailyReminder(this, "12:09","Go Check Movie Review App Today!");
         } else {
-          Toast.makeText(ReminderActivity.this, "getString(R.string.new_release_disabled)", Toast.LENGTH_SHORT).show();
+          Toast.makeText(this, "getString(R.string.daily_disabled)", Toast.LENGTH_SHORT).show();
+          reminderReceiver.cancelDailyReminder(this);
         }
-      }
-    });
+        break;
+      case R.id.switch_new_release:
+        if (switchNewRelease.isChecked()) {
+          String newReleaseMovie = ReminderReceiver.EXTRA_MESSAGE;
+          log.d("newReleaseMovie", newReleaseMovie);
+//          Toast.makeText(this, "getString(R.string.new_release_enabled)", Toast.LENGTH_SHORT).show();
+          reminderReceiver.setNewReleaseReminder(this, "15:36",newReleaseMovie);
+        } else {
+          Toast.makeText(this, "getString(R.string.new_release_disabled)", Toast.LENGTH_SHORT).show();
+          reminderReceiver.cancelNewReleaseReminder(this);
+        }
+        break;
+    }
   }
 }
---
+++ app/src/main/java/com/fyfirman/moviecatalogue/feature/reminder/ReminderReceiver.java
@@ -0,0 +1,234 @@
+package com.fyfirman.moviecatalogue.feature.reminder;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import com.fyfirman.moviecatalogue.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import cz.msebera.android.httpclient.Header;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ReminderReceiver extends BroadcastReceiver {
+
+  public static final String api_key = "b8d05a72822a6e82cf552ec84fc42c85";
+  public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+  private String title, message;
+
+  public ReminderReceiver() {
+  }
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    message = intent.getStringExtra(EXTRA_MESSAGE);
+
+    if (message.equalsIgnoreCase("EXTRA_MESSAGE")) {
+      getNewRelease(context);
+    } else { //Daily reminder
+      title = "Movie Review";
+      showReminderNotification(context, title, message);
+    }
+  }
+
+  public boolean isDateInvalid(String date, String format) {
+    try {
+      DateFormat df = new SimpleDateFormat(format, Locale.getDefault());
+      df.setLenient(false);
+      df.parse(date);
+      return false;
+    } catch (ParseException e) {
+      return true;
+    }
+  }
+
+  private void showReminderNotification(Context context, String title, String message) {
+    String CHANNEL_ID = "Channel_1";
+    String CHANNEL_NAME = "reminderManager channel";
+
+    NotificationManager notificationManagerCompat = (NotificationManager) context
+        .getSystemService(Context.NOTIFICATION_SERVICE);
+    Uri reminderSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_movie_notif)
+        .setContentTitle(title)
+        .setContentText(message)
+        .setColor(ContextCompat.getColor(context, android.R.color.transparent))
+        .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+        .setSound(reminderSound);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+          CHANNEL_NAME,
+          NotificationManager.IMPORTANCE_DEFAULT);
+      channel.enableVibration(true);
+      channel.setVibrationPattern(new long[]{1000, 1000, 1000, 1000, 1000});
+      builder.setChannelId(CHANNEL_ID);
+      if (notificationManagerCompat != null) {
+        notificationManagerCompat.createNotificationChannel(channel);
+      }
+    }
+
+    Notification notification = builder.build();
+
+    if (notificationManagerCompat != null) {
+      notificationManagerCompat.notify(101, notification);
+    }
+  }
+
+  public void setDailyReminder(Context context, String time, String message) {
+    String TIME_FORMAT = "HH:mm";
+
+    if (isDateInvalid(time, TIME_FORMAT)) {
+      return;
+    }
+
+    AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+    Intent intent = new Intent(context, ReminderReceiver.class);
+    intent.putExtra(EXTRA_MESSAGE, message);
+
+    String[] timeArray = time.split(":");
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]));
+    calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]));
+    calendar.set(Calendar.SECOND, 0);
+
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 101, intent, 0);
+
+    if (alarmManager != null) {
+      alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+          AlarmManager.INTERVAL_DAY, pendingIntent);
+    }
+  }
+
+  public void setNewReleaseReminder(Context context, String time, String message) {
+    String TIME_FORMAT = "HH:mm";
+
+    if (isDateInvalid(time, TIME_FORMAT)) {
+      return;
+    }
+
+    AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+    Intent intent = new Intent(context, ReminderReceiver.class);
+    intent.putExtra(EXTRA_MESSAGE, message);
+
+    String[] timeArray = time.split(":");
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]));
+    calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]));
+    calendar.set(Calendar.SECOND, 0);
+
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 101, intent, 0);
+
+    if (alarmManager != null) {
+      alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+          AlarmManager.INTERVAL_DAY, pendingIntent);
+    }
+  }
+//  public void setNewReleaseReminder(Context context, String time, String message) {
+//    Log.d("New Release Reminder", "Opened");
+//    String TIME_FORMAT = "HH:mm";
+//
+//    if (isDateInvalid(time, TIME_FORMAT)) {
+//      return;
+//    }
+//
+//    AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+//    Intent intent = new Intent(context, ReminderReceiver.class);
+//    intent.putExtra(EXTRA_MESSAGE, message);
+//
+//    String[] timeArray = time.split(":");
+//
+//    Calendar calendar = Calendar.getInstance();
+//    calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]));
+//    calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]));
+//    calendar.set(Calendar.SECOND, 0);
+//
+//    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 102, intent, 0);
+//
+//    if (alarmManager != null) {
+//      alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+//          AlarmManager.INTERVAL_DAY, pendingIntent);
+//      Log.d("New Release Reminder", "Setted");
+//    }
+//  }
+
+  public void cancelDailyReminder(Context context) {
+    AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+    Intent intent = new Intent(context, ReminderReceiver.class);
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 101, intent, 0);
+    pendingIntent.cancel();
+
+    if (alarmManager != null) {
+      alarmManager.cancel(pendingIntent);
+    }
+  }
+
+  public void cancelNewReleaseReminder(Context context) {
+    AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+    Intent intent = new Intent(context, ReminderReceiver.class);
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 102, intent, 0);
+    pendingIntent.cancel();
+
+    if (alarmManager != null) {
+      alarmManager.cancel(pendingIntent);
+    }
+  }
+
+  private void getNewRelease(final Context context) {
+    AsyncHttpClient client = new AsyncHttpClient();
+
+    SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    String currentDate = date.format(new Date());
+
+    String url = "https://api.themoviedb.org/3/discover/movie?api_key=" + api_key + "&primary_release_date.gte="
+        + currentDate + "&primary_release_date.lte=" + currentDate;
+
+    client.get(url, new AsyncHttpResponseHandler() {
+      @Override
+      public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+        try {
+          String result = new String(responseBody);
+          JSONObject responseObject = new JSONObject(result);
+          JSONArray list = responseObject.getJSONArray("results");
+
+          title = "New Released Movie";
+          message = "";
+
+          for (int i = 0; i < list.length(); i++) {
+            JSONObject releasedMovie = list.getJSONObject(i);
+            message = message + releasedMovie.getString("title") + ", ";
+          }
+
+          showReminderNotification(context, title, message);
+        } catch (Exception e) {
+          Log.d("movieNewReleaseErr", e.getMessage());
+        }
+      }
+
+      @Override
+      public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+        Log.d("movieNewReleaseNoConn", error.getMessage());
+      }
+    });
+  }
+}
